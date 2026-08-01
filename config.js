4a5240b743f646c68ce1a0076c84dddf685f7527
hiythjssj <hiythjssj@gmail.com>
2026-08-02 01:33:49 +0800

    Enhance date-aware plan system

--- config.js
+++
@@ -1,6 +0,0 @@
-// 晓的工作台云同步配置
-// Publishable key 用于浏览器前端。不要在此文件中放 secret/service_role key。
-window.XIAO_CLOUD_CONFIG = {
-  supabaseUrl: 'https://kplrdvbzeedwprhxwahz.supabase.co',
-  supabaseAnonKey: 'sb_publishable_OWVy7qG7QlBNY2mZGaWtbg_0s_29cMK'
-};
