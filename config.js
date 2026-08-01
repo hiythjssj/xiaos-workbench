d22e1767d24992ed738014c64fd5777f7a53b4e0
hiythjssj <hiythjssj@gmail.com>
2026-08-02 01:17:31 +0800

    Connect workbench to Supabase project

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 // 晓的工作台云同步配置
-// 在 Supabase 项目 Settings > API 中复制 Project URL 和 anon public key 到下面。
-// anon key 可以用于浏览器前端，切勿把 service_role key 放到这里。
+// Publishable key 用于浏览器前端。不要在此文件中放 secret/service_role key。
 window.XIAO_CLOUD_CONFIG = {
-  supabaseUrl: '',
-  supabaseAnonKey: ''
+  supabaseUrl: 'https://kplrdvbzeedwprhxwahz.supabase.co',
+  supabaseAnonKey: 'sb_publishable_OWVy7qG7QlBNY2mZGaWtbg_0s_29cMK'
 };
