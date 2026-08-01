44754e301e38cb938779135bb938676339544c86
hiythjssj <hiythjssj@gmail.com>
2026-08-02 00:48:19 +0800

    Add Supabase cloud configuration template

---
+++ config.js
@@ -0,0 +1,7 @@
+// 晓的工作台云同步配置
+// 在 Supabase 项目 Settings > API 中复制 Project URL 和 anon public key 到下面。
+// anon key 可以用于浏览器前端，切勿把 service_role key 放到这里。
+window.XIAO_CLOUD_CONFIG = {
+  supabaseUrl: '',
+  supabaseAnonKey: ''
+};
